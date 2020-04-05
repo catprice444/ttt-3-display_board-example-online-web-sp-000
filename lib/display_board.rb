a9ff213759e4e0539a2f2e2079fd0d330440d079
Catherine Price <catprice444@gmail.com>
2020-04-05 01:04:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,9 @@
-puts "   |   |   "
-puts "___________"
-puts "   |   |   "
-puts "___________"
-puts "   |   |   "
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  puts "   |   |   "
+  puts "___________"
+  puts "   |   |   "
+  puts "___________"
+  puts "   |   |   "
+end
+
+display_board
